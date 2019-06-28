0ccf5086fefa62ba4f06f61d1fa94309970526bf
fidansamet <fidansamet@hacettepe.edu.tr>
2019-06-28 23:52:11 +0300

    Bricks created

--- tetris/ViewController.swift
+++ tetris/ViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +16,5 @@ class ViewController: UIViewController {
     
     }
 
-    
 }
 
